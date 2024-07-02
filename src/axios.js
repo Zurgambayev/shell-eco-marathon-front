6aa46dd8cfdaa770c48413d188c9ffaa196203bc
Zurgambayev <polveko50@mail.ru>
2024-07-02 22:12:19 +0800

    -m

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3050', // замените на ваш базовый URL
+  baseURL: process.env.REACT_APP_API_URL, // замените на ваш базовый URL
 });
 
 instance.interceptors.request.use((config) => {
